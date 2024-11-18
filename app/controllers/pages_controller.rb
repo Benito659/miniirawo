8f04274d800947326605fc3f647fa6beee1df5be
Benito659 <whabeto@gmail.com>
2024-11-18 03:43:33 +0100

    ajouts de toutes les vues Ressources, Produit, Espace personnelle et Admin

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,16 @@ class PagesController<ApplicationController
     def home 
 
     end
+
+    def produit
+
+    end
+
+    def espacepersonnelle
+
+    end
+
+    def admin
+
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
 
   root "pages#home"
 
+  get "/produit", to: "pages#produit"
+
+  get "/admin", to: "pages#admin"
+
+  get "/espacepersonnelle", to: "pages#espacepersonnelle"
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
