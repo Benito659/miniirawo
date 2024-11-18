2358606761ef34678035133ee4c66d562e20b198
Benito659 <whabeto@gmail.com>
2024-11-18 18:28:58 +0100

    Creation des modèles pour db

--- app/models/ressource.rb
+++ app/models/ressource.rb
@@ -1,3 +1,4 @@
 class Ressource < ApplicationRecord
-
+    has_many :achats, foreign_key: :produit_id
+    has_many :visiteurs, through: :achats
 end
--- app/models/visiteur.rb
+++ app/models/visiteur.rb
@@ -1,3 +1,4 @@
 class Visiteur < ApplicationRecord
-
+    has_many :achats, foreign_key: 'visiteur_id'
+    has_many :ressources, through: :achats, source: :ressource
 end
